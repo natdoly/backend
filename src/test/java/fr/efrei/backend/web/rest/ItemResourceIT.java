594466d60c404b6faeb5473c9d9ce319dbef3f2b
natdoly <61319301+natdoly@users.noreply.github.com>
2023-11-29 14:36:46 +0100

    update itemResourceIT

--- src/test/java/fr/efrei/backend/web/rest/ItemResourceIT.java
+++ src/test/java/fr/efrei/backend/web/rest/ItemResourceIT.java
@@ -1,11 +1,14 @@
 package fr.efrei.backend.web.rest;
 
+import fr.efrei.backend.domain.Item;
 import fr.efrei.backend.repo.ItemRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -20,9 +23,13 @@ public class ItemResourceIT {
     @Transactional
     void createItem() throws Exception {
         int databaseSizeBeforeCreate = itemRepository.findAll().size();
-        assertThat(databaseSizeBeforeCreate).isEqualTo(6);
+        assertThat(databaseSizeBeforeCreate).isEqualTo(0);
 
-      /*  List<Item> itemList = itemRepository.findAll();
-        assertThat(itemList).hasSize(databaseSizeBeforeCreate + 1);*/
+        Item item = new Item();
+        item.setName("nathan");
+        itemRepository.save(item);
+
+        List<Item> itemList = itemRepository.findAll();
+        assertThat(itemList).hasSize(databaseSizeBeforeCreate + 1);
     }
 }
